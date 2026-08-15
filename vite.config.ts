28f0bf9470230b4e55bf0619425e6eb8f9f078fe
OmarTamer512 <omar.tamer@insharo.consulting>
2026-08-15 16:16:58 +0300

    fixing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-})
+  base: "/Basma-Gado-Portfolio-/",
+});
